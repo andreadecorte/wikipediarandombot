c7509fa7134b454975e74972fe813eb82123450a
Andrea Decorte <104198+klenje@users.noreply.github.com>
2021-02-07 22:31:46 +0100

    Add basic bot skeleton

---
+++ cmd/wikipediarandombot/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	bot "github.com/klenje/wikipediarandombot/pkg/bot"
+	log "github.com/sirupsen/logrus"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"os"
+	"time"
+)
+
+func main() {
+
+	var (
+		use_webhook           = false
+		verbose               = true
+		port                  = os.Getenv("PORT")
+		publicURL             = os.Getenv("PUBLIC_URL")
+		tg_token              = os.Getenv("TELEGRAM_TOKEN")
+		poller      tb.Poller = nil
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	poller = &tb.LongPoller{Timeout: 10 * time.Second}
+	// Webhook needs a public URL
+	if use_webhook == true && publicURL != "" {
+		log.Info("Using webhook")
+		poller = webhook
+	}
+
+	if tg_token == "" {
+		log.Fatal("Telegram token missing, check README")
+	}
+
+	b, err := tb.NewBot(tb.Settings{
+		Token:     tg_token,
+		Poller:    poller,
+		Verbose:   verbose,
+		ParseMode: tb.ModeHTML,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if use_webhook == false {
+		// If we switch from webhook to poller
+		// we may need to delete the webhook
+		b.RemoveWebhook()
+	}
+
+	h := bot.New(bot.Config{
+		Bot: b,
+	})
+
+
+	b.Handle("/help", h.OnHelp)
+	b.Handle("/start", h.OnHelp)
+
+	//b.Handle(tb.OnText, h.OnText)
+	//b.Handle(tb.OnQuery, h.OnInlineQuery)
+
+	log.Info("Start bot")
+	b.Start()
+}
